e2fb7139713bb856241848840b4e5715880d2eca
bethel014 <144551342+bethel014@users.noreply.github.com>
2023-12-02 07:04:12 +0100

    Added search city function

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ function handleSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#weather-input");
-  console.log(searchInput);
+
+  let cityInput = document.querySelector("#city");
+  console.log(cityInput);
+  cityInput.innerHTML = searchInput.value;
 }
