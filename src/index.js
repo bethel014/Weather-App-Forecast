e880fbc335d168caed442f9f53d2d209b3fb7d25
bethel014 <144551342+bethel014@users.noreply.github.com>
2023-12-02 18:27:32 +0100

    modified  CSS and HTML file, used the integrated Api to get the current conditions of the weather

--- src/index.js
+++ src/index.js
@@ -5,19 +5,37 @@ function handleSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#weather-input");
-  let city = searchInput.value;
+  displayCity(searchInput.value);
+}
+
+function displayCity(city) {
   let apiKey = "8fa2ab32e21db893o44btbabb185f06b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
   axios.get(apiUrl).then(displayCurrentTemp);
 }
 
 function displayCurrentTemp(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
   let countryElement = document.querySelector("#country");
   countryElement.innerHTML = response.data.country;
+
   let temperature = document.querySelector("#weather-value");
   temperature.innerHTML = Math.round(response.data.temperature.current);
+
+  let humidityElement = document.querySelector("#value");
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/hr`;
+
+  let condition = document.querySelector(".weatherDetail");
+  condition.innerHTML = response.data.condition.description;
+
+  let weatherIcon = document.querySelector(".icon");
+  weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}" class ="weather-app-icon" />`;
 }
 
 let currentDate = new Date();
@@ -44,3 +62,5 @@ if (hour < 10) {
 
 let weatherDate = document.querySelector("#current-date");
 weatherDate.innerHTML = `${day} ${hour} : ${minute} ,`;
+
+displayCity("Asaba");
