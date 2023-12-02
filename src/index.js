8bf175cc844f20615cae40c437d3a86402209320
bethel014 <144551342+bethel014@users.noreply.github.com>
2023-12-02 08:02:45 +0100

    Got the current Date for the app

--- src/index.js
+++ src/index.js
@@ -5,8 +5,32 @@ function handleSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#weather-input");
-
   let cityInput = document.querySelector("#city");
   console.log(cityInput);
   cityInput.innerHTML = searchInput.value;
 }
+
+let currentDate = new Date();
+
+let minute = currentDate.getMinutes();
+let hour = currentDate.getHours();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let weatherDate = document.querySelector("#current-date");
+weatherDate.innerHTML = `${day} ${hour} : ${minute} ,`;
